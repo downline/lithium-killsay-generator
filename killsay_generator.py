4c732567d0cbc82df4e891bf468daba885d8da81
downline <57864637+downline@users.noreply.github.com>
2020-11-08 17:05:18 +0500

    Update killsay_generator.py

--- killsay_generator.py
+++ killsay_generator.py
@@ -1,4 +1,3 @@
-
 file = "killsays.txt"
 lithium_file = "lithium_killsays.cfg"
     
@@ -6,22 +5,19 @@ def replace_foreign_shit(line): # In case of your killsays having foreign variab
    im_stuff = line.rstrip().replace("%victim%", "%player_name%")
    return im_stuff
 
-def main():
-    try:
-        f = open(file, "r")
-        f_lithium = open(lithium_file, "w+")
-        killsays = []
-        count_of_killsays = 0
-        for line in f:
-            count_of_killsays += 1
-            killsays.append('alias lithium_killsay' + str(count_of_killsays) + ' "say ' + replace_foreign_shit(line) + '"\n')
-            f_lithium.write(killsays[count_of_killsays - 1])
-        f.close()
-        f_lithium.close()
-        print("Number of killsays saved: ", count_of_killsays)
-    except FileNotFoundError:
-        print("You didn't create killsays.txt file.")
-    except Exception as e:
-        print("Unexpected error: %s" % e)
-if __name__== "__main__":
-    main()
+try:
+    f = open(file, "r")
+    f_lithium = open(lithium_file, "w+")
+    killsays = []
+    count_of_killsays = 0
+    for line in f:
+        count_of_killsays += 1
+        killsays.append('alias lithium_killsay' + str(count_of_killsays) + ' "say ' + replace_foreign_shit(line) + '"\n')
+        f_lithium.write(killsays[count_of_killsays - 1])
+    f.close()
+    f_lithium.close()
+    print("Number of killsays saved: ", count_of_killsays)
+except FileNotFoundError:
+    print("You didn't create killsays.txt file.")
+except Exception as e:
+    print("Unexpected error: %s" % e)
